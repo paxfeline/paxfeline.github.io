72cc4349061461b25d1d6e9a6fb9046550971ed5
paxfeline <paxfeline@gmail.com>
2020-04-09 15:56:54 -0700

    added spreadsheet

---
+++ expense-spreadsheet/precache-manifest.bae02d3575ccc609e9b204215053d675.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90a19b46aa39063415760e52df2e57ed",
+    "url": "/expense-spreadsheet/index.html"
+  },
+  {
+    "revision": "e648ca291c50772c8481",
+    "url": "/expense-spreadsheet/static/css/main.775be228.chunk.css"
+  },
+  {
+    "revision": "978422f7d75ee345d722",
+    "url": "/expense-spreadsheet/static/js/2.b3cf3782.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-spreadsheet/static/js/2.b3cf3782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e648ca291c50772c8481",
+    "url": "/expense-spreadsheet/static/js/main.b6330502.chunk.js"
+  },
+  {
+    "revision": "aecefa0977c0069bd65f",
+    "url": "/expense-spreadsheet/static/js/runtime-main.8ebec876.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/expense-spreadsheet/static/media/logo.5d5d9eef.svg"
+  }
+]);
